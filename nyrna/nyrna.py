5ef156998e8a5e452ace541a810b65f867d7f093
Kristen McWilliam <merritt_public@outlook.com>
2020-06-08 11:06:31 -0400

    Completely refactored into a class, cleaned up code, improved comments and documentation

--- nyrna/nyrna.py
+++ nyrna/nyrna.py
@@ -42,12 +42,16 @@ Make sure AppKit is available
 """
 
 
+# Standard Library
 import logging
 import logging.handlers
-import sys
-import psutil
-import os
 import pickle
+import sys
+import os
+
+
+# Third Party Libraries
+import psutil
 
 
 # Since this runs via hotkey there is no terminal to
@@ -69,21 +73,63 @@ def log(logMessage):
     return logger.debug(logMessage)
 
 
-def get_active_window():
-
-    # TODO: Refactor to seperate classes / functions / files / etc.
+class Process:
 
     """
-    Get the currently active window and accompanying process information.
-
-    Returns a dictionary (return_values) with the information, eg. PID, process name, process status, etc.
+    Object representing the process for which 
+    we want to toggle suspend / resume.
     """
 
-    return_values = {}
+    def __init__(self):
+        self.operating_system = self.get_operating_system()
+        self.process = self.check_for_saved_process()
 
-    active_window_name = None
+    def get_operating_system(self):
+        """ Determine the operating system """
+        if sys.platform in ["linux", "linux2"]:
+            return "linux"
+        elif sys.platform in ["Windows", "win32", "cygwin"]:
+            return "windows"
+        elif sys.platform in ["Mac", "darwin", "os2", "os2emx"]:
+            # return "mac"
+            log("OS 'Mac' is not currently supported!")
+            raise Exception
+        else:
+            log("Unable to determine operating system.")
+            log("sys.platform is:")
+            log(sys.platform)
+            raise Exception
 
-    if sys.platform in ["linux", "linux2"]:
+    def check_for_saved_process(self):
+        """ Check if a saved process file exists from a previous suspend """
+        try:
+            saved_process_file = open(os.path.join(sys.path[0], "paused_app.pkl"), "rb")
+            saved_process = pickle.load(saved_process_file)  # Saved dictionary
+            log("Opened paused_app.pkl:")
+            log(saved_process)
+            saved_name = saved_process["name"]
+            saved_pid = saved_process["pid"]
+            log(f"saved_name: {saved_name}")
+            log(f"saved_pid: {saved_pid}")
+            # Check if same process still exists
+            process_name = str(psutil.Process(saved_pid).name())
+            log(f"process name: {process_name}")
+            if saved_name == process_name:
+                # Return the current process with an updated status
+                return psutil.Process(saved_pid)
+        except:
+            log("There is no paused_app.pkl, getting active window")
+            return self.get_active_window()
+
+    def get_active_window(self):
+        """ Call a method to find the window based on the OS """
+        if self.operating_system == "linux":
+            return self.get_active_window_linux()
+        elif self.operating_system == "windows":
+            return self.get_active_window_windows()
+
+    def get_active_window_linux(self):
+        """ Find the process of the active window on Linux """
         # Alternatives: http://unix.stackexchange.com/q/38867/4784
         try:
             import wnck
@@ -114,150 +160,84 @@ def get_active_window():
                 pid = active_window.get_pid()
                 with open("/proc/{pid}/cmdline".format(pid=pid)) as f:
                     active_window_name = f.read()
+        # Check if we found a Wine process with virtual desktop enabled
+        if "explorer.exe" not in active_window_name:
+            return psutil.Process(pid)
+        else:
+            return self.find_wine_process()
 
-    # Did we receive a Wine process with virtual desktop enabled?
-    # Suspending explorer.exe (virtual desktop setting) does nothing, so we need to find the real process.
-    if "explorer.exe" in active_window_name:
-        log(f"active_window_name: {active_window_name}")
-        explorer_parent_PID = psutil.Process(pid).ppid()
-        log(f"Parent PID: {explorer_parent_PID}")
-        parent_process = psutil.Process(explorer_parent_PID)
-        log(f"Parent process: {parent_process}")
-
-        # TODO: See if this is still needed now that the generic Wine method is available.
-        if "lutris" in psutil.Process(explorer_parent_PID).name():
-            lutris_child = psutil.Process(explorer_parent_PID).children()[0]
-            log(f"Lutris child: {lutris_child}")
-            lutris_child_PID = lutris_child.pid
-            log(f"Lutris child PID: {lutris_child_PID}")
-            return_values["pid"] = lutris_child_PID
-        else:  # Generic way to find the wine process
-            wineserver_processes = []
+    def find_wine_process(self):
+        """ Suspending explorer.exe (virtual desktop setting) 
+        does nothing, so we need to find the real process """
+        wineserver_processes = []
+        # Check if any running process is a wineserver
+        for process in psutil.process_iter():
+            if "wineserver" in process.name():
+                wineserver_processes.append(process)
+        if len(wineserver_processes) == 1:
+            wineserver_process = wineserver_processes[0]
+            wineserver_PID = wineserver_process.pid
+            # List files opened by wineserver,
+            # The executable we need should be one of them
+            files = psutil.Process(wineserver_PID).open_files()
+            file_path = None
+            # Find which file is the executable of the Windows program
+            for item in files:
+                if "exe" in str(item):
+                    file_path = str(item)
+            log(f"Wine - file_path: {file_path}")
+            # Parse the actual application name from all the path text
+            split_file_path = file_path.rsplit(".exe", 1)
+            split_file_path = split_file_path[0].split("/")
+            wine_app_name = split_file_path[-1]
+            log(f"Wine - split_file_path: {split_file_path}")
+            log(f"Wine - executable name: {wine_app_name}")
+            # Find a running process that contains the application name
             for process in psutil.process_iter():
-                if "wineserver" in process.name():
-                    wineserver_processes.append(process)
-            if len(wineserver_processes) == 1:
-                wineserver_process = wineserver_processes[0]
-                wineserver_PID = wineserver_process.pid
-                log(f"wineserver_PID: {wineserver_PID}")
-                wine_process_PID = find_wine_process(wineserver_PID)
-                return_values["pid"] = wine_process_PID
-            else:
-                log("Error: Found multiple wineserver processes")
+                if wine_app_name in process.name():
+                    wine_process = process
+            return wine_process
+        else:
+            # If there are multiple wineserver processes,
+            # how would we determine which is the one we need?
+            log("Error: Found multiple wineserver processes")
+            raise Exception
 
-    elif sys.platform in ["Windows", "win32", "cygwin"]:
-        # http://stackoverflow.com/a/608814/562769
-        import win32gui
+    def get_active_window_windows(self):
+        """ Find the process of the active window on Windows """
+        pass
 
-        window = win32gui.GetForegroundWindow()
-        active_window_name = win32gui.GetWindowText(window)
-    elif sys.platform in ["Mac", "darwin", "os2", "os2emx"]:
-        # http://stackoverflow.com/a/373310/562769
-        from AppKit import NSWorkspace
+    def toggle_suspend(self):
 
-        active_window_name = NSWorkspace.sharedWorkspace().activeApplication()[
-            "NSApplicationName"
-        ]
-    else:
-        print(
-            "sys.platform={platform} is unknown. Please report.".format(
-                platform=sys.platform
-            )
-        )
-        print(sys.version)
+        """
+        Toggle suspend/resume for the process.
+        After suspending, the process object will be saved to disk
+        so we can check if anything needs to be resumed in the case
+        that the suspended window can't be refocused.
+        """
 
-    if "pid" in return_values:
-        log("Process is Wine with virtual desktop")
-    else:
-        return_values["pid"] = pid
-
-    return_values["active_window_name"] = active_window_name
-
-    return return_values
-
-
-def find_wine_process(wineserver_PID):
-
-    """
-    Find the real Wine process when get_active_window()
-    finds explorer.exe (emulated virtual desktop).
-    
-    This is necessary because pausing explorer.exe has
-    no effect on the real Wine process.
-
-    Returns the PID of the actual Wine process.
-    """
-
-    # List files opened by wineserver
-    files = psutil.Process(wineserver_PID).open_files()
-
-    file_path = ""
-
-    # Find which file is the executable of the Windows program
-    for item in files:
-        if "exe" in str(item):
-            file_path = item
-
-    log(f"file_path: {file_path}")
-
-    file_path = str(file_path)
-
-    # Parse the actual application name
-    split_file_path = file_path.rsplit(".exe", 1)
-    split_file_path = split_file_path[0].split("/")
-    wine_app_name = split_file_path[-1]
-
-    log(f"split_file_path: {split_file_path}")
-    log(f"Wine app name: {wine_app_name}")
-
-    # Find a running process that contains the application name
-    for process in psutil.process_iter():
-        if wine_app_name in process.name():
-            wine_process_PID = process.pid
-
-    return wine_process_PID
+        if self.process.status() == "stopped":  # Resume
+            psutil.Process(self.process.pid).resume()
+            log(f"Resumed process has name: {self.process.name()}")
+            log(f"Resumed process has PID: {self.process.pid}")
+            # Remove the saved object from disk so as
+            # to not cause confusion for next time
+            os.remove(os.path.join(sys.path[0], "paused_app.pkl"))
+        else:  # Suspend
+            psutil.Process(self.process.pid).suspend()
+            log(f"Suspended process has name: {self.process.name()}")
+            log(f"Suspended process has PID: {self.process.pid}")
+            # Save the process we found, so we can check for which
+            # process to resume in case the window can't be focused.
+            # Not being able to focus after suspend has been a frequent issue with games.
+            save_values = {}  # Save to dict since we can't pickle the psutil object
+            save_values["pid"] = self.process.pid
+            save_values["name"] = self.process.name()
+            saved_process_file = open(os.path.join(sys.path[0], "paused_app.pkl"), "wb")
+            pickle.dump(save_values, saved_process_file, pickle.HIGHEST_PROTOCOL)
+            saved_process_file.close()
 
 
-def suspend_process():
+new_process = Process()
 
-    """
-    Toggle suspend/resume for the active, foreground window.
-    """
-
-    process_values = None
-
-    # Check if a saved process file exists from previous suspend.
-    try:
-        saved_process_file = open(os.path.join(sys.path[0], "paused_app.pkl"), "rb")
-        process_values = pickle.load(saved_process_file)
-        log("Opened paused_app.pkl:")
-        log(process_values)
-    except:
-        log("There is no paused_app.pkl, getting active window")
-        process_values = get_active_window()
-        log(process_values)
-
-    # Get the current process status.
-    pid = process_values["pid"]
-    process_values["process_status"] = psutil.Process(pid).status()
-    process_status = process_values["process_status"]
-    log(f"Process status: {process_status}")
-
-    # Suspend or resume the process.
-    if process_status == "stopped":
-        psutil.Process(process_values["pid"]).resume()
-        log(f"Resumed process: {pid}")
-        os.remove(os.path.join(sys.path[0], "paused_app.pkl"))
-    else:
-        psutil.Process(process_values["pid"]).suspend()
-        log(f"Suspended process: {pid}")
-
-        # Save the dictionary with the values we found, so we can check
-        # for which process to resume in case the window can't be focused.
-        # Not being able to focus after suspend has been a frequent issue with games.
-        saved_process_file = open(os.path.join(sys.path[0], "paused_app.pkl"), "wb")
-        pickle.dump(process_values, saved_process_file, pickle.HIGHEST_PROTOCOL)
-        saved_process_file.close()
-
-
-suspend_process()
+new_process.toggle_suspend()
