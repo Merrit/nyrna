ef4b88abb2d393aec561c63b829eaee49090402e
Merritt <9575627+Merrit@users.noreply.github.com>
2021-02-15 14:55:16 -0500

    Delete nyrna_test.dart
    Expect CI runs in a headless environment, will have to mock tests later

--- test/nyrna_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:nyrna/nyrna.dart';
-
-void main() {
-  test('Desktop is not null', () async {
-    var desktop = await Nyrna.currentDesktop;
-    expect(desktop.runtimeType, int);
-  });
-}
