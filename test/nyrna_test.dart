e8e1388ee0cdd5130d40e31e6646e8b3a2872a32
Merritt <9575627+Merrit@users.noreply.github.com>
2021-02-15 14:52:06 -0500

    Uncomment test for Nyrna() - expected to fail CI

--- test/nyrna_test.dart
+++ test/nyrna_test.dart
@@ -1,9 +1,9 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:nyrna/nyrna.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nyrna/nyrna.dart';
 
-// void main() {
-//   test('Desktop is not null', () async {
-//     var desktop = await Nyrna.currentDesktop;
-//     expect(desktop.runtimeType, int);
-//   });
-// }
+void main() {
+  test('Desktop is not null', () async {
+    var desktop = await Nyrna.currentDesktop;
+    expect(desktop.runtimeType, int);
+  });
+}
