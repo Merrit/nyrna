2d247691fa797a26a2014af68b7ff5cbde1c139b
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-03-05 15:07:52 -0500

    Workaround for Win32 so it doesn't show console

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -8,11 +8,46 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  // Attach to console when present (e.g., 'flutter run') or create a
-  // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+
+
+  // // Attach to console when present (e.g., 'flutter run') or create a
+  // // new console when running with a debugger.
+  // if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+  //   CreateAndAttachConsole();
+  // }
+
+
+/* ------------------------ Hidden Console Workaround ----------------------- */
+
+
+  // https://github.com/flutter/flutter/issues/47891#issuecomment-708850435
+  
+
+   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
+  } else {
+    STARTUPINFO si = { 0 };
+    si.cb = sizeof(si);
+    si.dwFlags = STARTF_USESHOWWINDOW;
+    si.wShowWindow = SW_HIDE;
+
+    PROCESS_INFORMATION pi = { 0 };
+    WCHAR lpszCmd[MAX_PATH] = L"powershell.exe";
+    if (::CreateProcess(NULL, lpszCmd, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | CREATE_NO_WINDOW, NULL, NULL, &si, &pi)) {
+      do {
+        if (::AttachConsole(pi.dwProcessId)) {
+          ::TerminateProcess(pi.hProcess, 0);
+          break;
+        }
+      } while (ERROR_INVALID_HANDLE == GetLastError());
+      ::CloseHandle(pi.hProcess);
+      ::CloseHandle(pi.hThread);
+    }
   }
+
+
+/* ----------------------------- Workaround end ----------------------------- */
+
 
   // Initialize COM, so that it is available for use in the library and/or
   // plugins.
