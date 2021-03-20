30b46465999fb38057e58b75936472cd56cafb51
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-03-20 08:49:47 -0400

    Refactor ArgumentParser class and tests

--- lib/arguments/argument_parser.dart
+++ lib/arguments/argument_parser.dart
@@ -1,47 +1,61 @@
 import 'package:args/args.dart';
 import 'package:nyrna/config.dart';
+import 'package:nyrna/logger/logger.dart';
 
+/// Parse command-line arguments.
+///
+/// `Toggle` flag available with `-t` or `--toggle`.
+///
+/// `Logger` flag available with `-l` or `--log`.
+///
+/// Both options available with `-tl`.
 class ArgumentParser {
-  ArgumentParser(this.args) {
-    _init();
-  }
+  ArgumentParser(this.args);
 
   final List<String> args;
 
   final _parser = ArgParser();
 
-  void _init() {
-    _setToggleFlag();
-    _setLoggerFlag();
+  Future<void> init() async {
+    _setFlags();
+    await _parse();
   }
 
-  void _setToggleFlag() {
+  void _setFlags() {
     _parser.addFlag(
       'toggle',
       abbr: 't',
       defaultsTo: false,
     );
-    try {
-      final results = _parser.parse(args);
-      final toggle = results.wasParsed('toggle');
-      if (toggle) Config.toggle = true;
-    } catch (e) {
-      print('Error parsing toggle flag: \n$e');
-    }
-  }
-
-  void _setLoggerFlag() {
     _parser.addFlag(
       'log',
       abbr: 'l',
       defaultsTo: false,
     );
+  }
+
+  Future<void> _parse() async {
+    // Parse toggle flag.
+    final results = _parser.parse(args);
     try {
-      final results = _parser.parse(args);
-      final logger = results.wasParsed('log');
-      if (logger) Config.log = true;
+      final toggle = results.wasParsed('toggle');
+      if (toggle) Config.toggle = true;
+    } catch (e) {
+      print('Error parsing toggle flag: \n$e');
+    }
+    // Parse log flag.
+    bool logger;
+    try {
+      logger = results.wasParsed('log');
     } catch (e) {
       print('Error parsing logger flag: \n$e');
+    }
+    if (logger) {
+      // Set environment variable.
+      Config.log = true;
+      // One-time initialization of the logger.
+      final logger = Logger.instance;
+      await logger.init();
     }
   }
 }
--- test/arguments/argument_parser_test.dart
+++ test/arguments/argument_parser_test.dart
@@ -1,43 +1,73 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nyrna/config.dart';
 import 'package:nyrna/arguments/argument_parser.dart';
+import 'package:nyrna/logger/logger.dart';
 
 void main() {
-  group(
-    'Toggle flag',
-    () {
-      setUp(() => Config.toggle = false);
+  setUp(() {
+    Config.log = false;
+    Config.toggle = false;
+  });
 
-      test(
-        'With no flags toggle is false',
-        () {
-          ArgumentParser([]);
-          expect(Config.toggle, false);
-        },
-      );
-      test(
-        '--toggle sets Config.toggle to true',
-        () {
-          ArgumentParser(['--toggle']);
-          expect(Config.toggle, true);
-        },
-      );
+  test('With no flags log is false', () async {
+    final parser = ArgumentParser([]);
+    await parser.init();
+    expect(Config.log, false);
+  });
 
-      test(
-        '-t sets Config.toggle to true',
-        () {
-          ArgumentParser(['-t']);
-          expect(Config.toggle, true);
-        },
-      );
+  test('Config.log is true with -l flag', () async {
+    final parser = ArgumentParser(['-l']);
+    await parser.init();
+    expect(Config.log, true);
+  });
 
-      test(
-        'With unknown flag Config.toggle returns false',
-        () {
-          ArgumentParser(['--taco']);
-          expect(Config.toggle, false);
-        },
-      );
+  test('Config.log is true with --log flag', () async {
+    final parser = ArgumentParser(['--log']);
+    await parser.init();
+    expect(Config.log, true);
+  });
+
+  test(
+    'With no flags toggle is false',
+    () async {
+      final parser = ArgumentParser([]);
+      await parser.init();
+      expect(Config.toggle, false);
+    },
+  );
+  test(
+    '--toggle sets Config.toggle to true',
+    () async {
+      final parser = ArgumentParser(['--toggle']);
+      await parser.init();
+      expect(Config.toggle, true);
+    },
+  );
+
+  test(
+    '-t sets Config.toggle to true',
+    () async {
+      final parser = ArgumentParser(['-t']);
+      await parser.init();
+      expect(Config.toggle, true);
+    },
+  );
+
+  test(
+    'With unknown flag Config.toggle returns false',
+    () async {
+      final parser = ArgumentParser(['--taco']);
+      await parser.init();
+      expect(Config.toggle, false);
+    },
+  );
+  test(
+    'With log and toggle flags log and toggle are true',
+    () async {
+      final parser = ArgumentParser(['-tl']);
+      await parser.init();
+      expect(Config.toggle, true);
+      expect(Config.log, true);
     },
   );
 }
