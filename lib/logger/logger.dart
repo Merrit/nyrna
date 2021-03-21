5c2ba5b8cff69ad4c19cd575c0e9b3573518e58a
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-03-21 13:14:15 -0400

    Refactor & comment Logger()

--- lib/logger/logger.dart
+++ lib/logger/logger.dart
@@ -7,16 +7,21 @@ class Logger {
   Logger._privateConstructor();
   static final Logger instance = Logger._privateConstructor();
 
+  /// System's temp dir, for example: `/tmp`.
   Directory _tempDir;
+
+  /// Absolute path to `nyrna.log`.
   File _logFile;
 
   Future<void> init() async {
     _tempDir = await p.getTemporaryDirectory();
     _logFile = File('${_tempDir.path}/nyrna.log');
+    // Clean up previous log, if it exists.
     final previousLog = await _logFile.exists();
     if (previousLog) await _logFile.delete();
   }
 
+  /// Write line(s) to the `nyrna.log` file.
   Future<void> log(Object object) async {
     await _logFile.writeAsString(
       '${DateTime.now()} $object'
@@ -25,7 +30,7 @@ class Logger {
     );
   }
 
-  // Call flush before exiting Nyrna to ensure log is written to disk.
+  /// Flush the log to ensure it has been written to disk before exiting.
   Future<void> flush(Object object) async {
     await _logFile.writeAsString(
       '\n'
