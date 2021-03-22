d800ba70af3d651dab0a1eff0443ca20158775f3
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-03-22 17:47:49 -0400

    Refactor & comment Window

--- lib/window/window.dart
+++ lib/window/window.dart
@@ -1,18 +1,25 @@
 import 'package:nyrna/window/window_controls.dart';
 
+/// Represents a visible window on the current desktop.
 class Window {
-  Window({this.title, this.pid, this.id});
+  Window({this.id, this.pid, this.title});
 
   final _windowControls = WindowControls();
 
-  String title;
+  /// The unique window ID number associated with this window.
+  final int id;
 
-  int pid;
+  /// The PID of the process associated with this window.
+  final int pid;
 
-  /// Unique window id from the hexadecimal integer.
-  int id;
+  /// The title of this window, often shown on the window's 'Title Bar'.
+  ///
+  /// Can & does change, for example a browser shows the title of the page.
+  final String title;
 
+  /// Minimize this window.
   Future<void> minimize() async => await _windowControls.minimize(id);
 
+  /// Restore (un-minimize) this window.
   Future<void> restore() async => await _windowControls.restore(id);
 }
