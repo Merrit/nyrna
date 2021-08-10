ba6e9891b00c6487869c37377c1ce8002b09db28
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-08-10 12:30:38 -0400

    Remove unneeded import

--- lib/presentation/preferences/widgets/behaviour_section.dart
+++ lib/presentation/preferences/widgets/behaviour_section.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nyrna/application/preferences/cubit/preferences_cubit.dart';
