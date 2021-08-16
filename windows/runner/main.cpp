9866b27dd5deca1703aa91be4383d55674a1b2f8
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-08-16 11:12:32 -0400

    Reduce default window size

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -64,7 +64,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(&run_loop, project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(800, 720);
+  Win32Window::Size size(530, 600);
   if (!window.CreateAndShow(L"Nyrna", origin, size)) {
     return EXIT_FAILURE;
   }
