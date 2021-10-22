590b7fa2917cbbedc18323afb6a1e6891ffb9070
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-10-22 16:49:31 -0400

    Add test for activeWindow

--- packages/native_platform/test/src/native_platform_test.dart
+++ packages/native_platform/test/src/native_platform_test.dart
@@ -1,11 +1,26 @@
+import 'dart:io' show Platform;
+
 import 'package:native_platform/src/native_platform.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final environmentVariables = Platform.environment;
+
+  /// Tests that require a display & windows to work with will
+  /// check for [runningInCI], and skip runs in GitHub Workflows.
+  final bool runningInCI = (environmentVariables['GITHUB_ACTIONS'] == 'true');
+
   final platform = NativePlatform();
 
   test('currentDesktop has a return value', () async {
     final desktop = await platform.currentDesktop();
     expect(desktop, isA<int>());
+  });
+
+  test('activeWindow returns a window', () async {
+    if (runningInCI) return;
+    final activeWindow = await platform.activeWindow();
+    expect(activeWindow.id, isPositive);
+    expect(activeWindow.pid, isPositive);
   });
 }
