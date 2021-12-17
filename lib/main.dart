33d87f549d1f19c8f77119fbb62192d08af2fe3b
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-12-17 13:57:56 -0500

    Sort imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:native_platform/native_platform.dart';
-import 'package:nyrna/infrastructure/logger/app_logger.dart';
-import 'package:nyrna/infrastructure/versions/versions.dart';
-import 'package:nyrna/presentation/app_widget.dart';
-import 'package:nyrna/infrastructure/preferences/preferences.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'application/app/app.dart';
 import 'application/preferences/cubit/preferences_cubit.dart';
 import 'application/theme/cubit/theme_cubit.dart';
 import 'domain/arguments/argument_parser.dart';
+import 'infrastructure/logger/app_logger.dart';
+import 'infrastructure/preferences/preferences.dart';
+import 'infrastructure/versions/versions.dart';
+import 'presentation/app_widget.dart';
 
 Future<void> main(List<String> args) async {
   // Parse command-line arguments.
