9895e0bc0fdd4660535bac019c4d531f3d892d91
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-12-23 09:20:05 -0500

    Make toggle an argument
    Refactor so toggle active is an argument and not a separate executable,
    only on Linux.
    
    Include help text from CLI

--- packages/active_window/lib/active_window.dart
+++ packages/active_window/lib/active_window.dart
@@ -1,4 +1,23 @@
+/// The portion of Nyrna that will act as a toggle to
+/// suspend / resume the active, foreground window.
+///
+/// Calling this should be bound to a hotkey
+/// using the operating system's default methods on Linux, or by using
+/// the included `toggle_active_hotkey.exe` on Windows which sits in the
+/// system tray and listens for the `Pause` keyboard key to activate.
+///
+/// When the hotkey launches Nyrna with the `--toggle` argument it will:
+/// - Find the active, foreground window
+/// - Minimize the window
+/// - Suspend the process that owns the window
+/// - Save the PID of said process & window id to a file
+///
+/// On subsequent launch when the file with a saved PID is found:
+/// - Resume the suspended process
+/// - Restore / unminimize the associated window
+/// - Delete the file containing the PID & window id so next call will suspend.
 library active_window;
 
 export 'src/active_window_handler.dart';
 export 'src/logger.dart';
+export 'src/toggle_active_window.dart';
