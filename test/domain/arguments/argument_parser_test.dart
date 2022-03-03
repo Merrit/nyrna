72f5ec41b0347fc92966a4cc468dea396c04d8d4
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-03-03 14:09:42 -0500

    Add ArgParser tests

---
+++ test/domain/arguments/argument_parser_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nyrna/domain/arguments/argument_parser.dart';
+
+void main() {
+  group('ArgumentParser:', () {
+    var argParser = ArgumentParser();
+
+    setUp(() => argParser = ArgumentParser());
+
+    test('toggle defaults to false', () {
+      final args = <String>[];
+      argParser.parseArgs(args);
+      expect(argParser.toggleActiveWindow, false);
+    });
+
+    test('--toggle works', () {
+      final args = ['--toggle'];
+      argParser.parseArgs(args);
+      expect(argParser.toggleActiveWindow, true);
+    });
+
+    test('-t works', () {
+      final args = ['-t'];
+      argParser.parseArgs(args);
+      expect(argParser.toggleActiveWindow, true);
+    });
+
+    test('log defaults to false', () {
+      final args = <String>[];
+      argParser.parseArgs(args);
+      expect(argParser.logToFile, false);
+    });
+
+    test('--log works', () {
+      final args = ['--log'];
+      argParser.parseArgs(args);
+      expect(argParser.logToFile, true);
+    });
+
+    test('-l works', () {
+      final args = ['-l'];
+      argParser.parseArgs(args);
+      expect(argParser.logToFile, true);
+    });
+  });
+}
