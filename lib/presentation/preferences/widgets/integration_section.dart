eefb9267ba76344f5907437dcd227d92c08f75e8
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-03-04 13:10:22 -0500

    Remove integration for Linux portable builds
    This is better handled by packages

--- lib/presentation/preferences/widgets/integration_section.dart
+++ lib/presentation/preferences/widgets/integration_section.dart
@@ -2,9 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:nyrna/application/app/app.dart';
-import 'package:nyrna/application/preferences/cubit/preferences_cubit.dart';
 
+import '../../../application/preferences/cubit/preferences_cubit.dart';
 import '../../styles.dart';
 
 /// Add shortcuts and icons for portable builds or autostart.
@@ -16,7 +15,7 @@ class IntegrationSection extends StatelessWidget {
     if (Platform.isWindows) {
       return const _WindowsIntegration();
     } else {
-      return const _LinuxIntegration();
+      return const SizedBox();
     }
   }
 }
@@ -47,65 +46,6 @@ class _WindowsIntegration extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class _LinuxIntegration extends StatelessWidget {
-  const _LinuxIntegration({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<AppCubit, AppState>(
-      builder: (context, state) {
-        return state.isPortable
-            ? Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Spacers.verticalMedium,
-                  const Text('System Integration'),
-                  Spacers.verticalXtraSmall,
-                  ListTile(
-                    leading: const Icon(Icons.add_circle_outline),
-                    title: const Text('Add Nyrna to launcher'),
-                    onTap: () => _confirmAddToLauncher(context),
-                  ),
-                ],
-              )
-            : const SizedBox();
-      },
-    );
-  }
-
-  /// Confirm with the user before adding .desktop and icon files.
-  void _confirmAddToLauncher(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          insetPadding: const EdgeInsets.symmetric(
-            horizontal: 150.0,
-            vertical: 24.0,
-          ),
-          content: const Text('This will add a menu item to the system '
-              'launcher with an associated icon so launching Nyrna is easier.'
-              '\n\n'
-              'Continue?'),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () async {
-                await preferencesCubit.createLauncher();
-                Navigator.pop(context);
-              },
-              child: const Text('Continue'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
