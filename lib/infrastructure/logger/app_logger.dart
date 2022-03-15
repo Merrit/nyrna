9916ed56710ddeba4651679038facbef993cbab1
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-03-15 11:00:21 -0400

    Use debugPrint to output logs to console

--- lib/infrastructure/logger/app_logger.dart
+++ lib/infrastructure/logger/app_logger.dart
@@ -1,19 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
 import 'log_file.dart';
 
 class AppLogger {
-  final _log = Logger('AppLogger');
-
   /// Print log messages & add them to a Queue so they can be referenced, for
   /// example from the [LogPage].
   void initialize() {
     final logQueue = LogFile.logs;
+    Logger.root.level = Level.ALL;
     Logger.root.onRecord.listen((record) {
       var msg = '${record.level.name}: ${record.time}: '
           '${record.loggerName}: ${record.message}';
       if (record.error != null) msg += '\nError: ${record.error}';
-      _log.info(msg);
+      debugPrint(msg);
       logQueue.addLast(record);
       // In case the log grows too crazy, prune for sanity.
       while (logQueue.length > 100) {
