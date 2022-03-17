6f911b05ea9cd345ccc1b4c2bd3cc3d9780b7a4e
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-03-17 11:13:50 -0400

    Refactor CustomAppBar

--- lib/presentation/app/widgets/custom_app_bar.dart
+++ lib/presentation/app/widgets/custom_app_bar.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:nyrna/application/app/app.dart';
-import 'package:nyrna/application/preferences/cubit/preferences_cubit.dart';
-import 'package:nyrna/presentation/preferences/preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import '../../../application/app/app.dart';
+import '../../../application/preferences/cubit/preferences_cubit.dart';
+import '../../preferences/preferences.dart';
 import '../app.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -32,9 +32,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         ),
         IconButton(
           icon: const Icon(Icons.settings),
-          onPressed: () {
-            Navigator.pushNamed(context, PreferencesPage.id);
-          },
+          onPressed: () => Navigator.pushNamed(context, PreferencesPage.id),
         ),
       ],
     );
@@ -46,9 +44,10 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     final currentVersion = state.runningVersion;
     final latestVersion = state.updateVersion;
     const url = 'https://nyrna.merritt.codes/download';
-    var _message = 'An update for Nyrna is available!\n\n'
+    final _message = 'An update for Nyrna is available!\n\n'
         'Current version: $currentVersion\n'
         'Latest version: $latestVersion';
+
     await showDialog(
       context: context,
       builder: (context) {
