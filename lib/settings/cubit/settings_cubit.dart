3b80c48c9e77d5f795f924cd07281b979257b24f
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-03-29 14:43:41 -0400

    Refactor helpers

--- lib/settings/cubit/settings_cubit.dart
+++ lib/settings/cubit/settings_cubit.dart
@@ -4,7 +4,7 @@ import 'dart:ui';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-import 'package:nyrna/application/helpers/json_converters.dart';
+import 'package:nyrna/helpers/json_converters.dart';
 import 'package:nyrna/infrastructure/icon_manager/icon_manager.dart';
 import 'package:nyrna/settings/settings_service.dart';
 import 'package:nyrna/theme/styles.dart';
