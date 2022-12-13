7286e34b07ef6dc8ab76fb97d99370e45a8258b8
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-12-13 12:51:03 -0500

    refactor: wrap url_launcher with UrlLauncher class

---
+++ lib/url_launcher/src/src.dart
@@ -0,0 +1,1 @@
+export 'url_launcher.dart';
--- test/app/cubit/app_cubit_test.dart
+++ test/app/cubit/app_cubit_test.dart
@@ -1,42 +1,27 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:nyrna/app/app.dart';
+import 'package:nyrna/url_launcher/url_launcher.dart';
 import 'package:test/test.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-class MockCanLaunchUrl extends Mock {
-  Future<bool> call(Uri url);
-}
-
-late CanLaunchUrlFunction mockCanLaunchUrl;
-
-class MockLaunchUrl extends Mock {
-  Future<bool> call(
-    Uri url, {
-    LaunchMode mode,
-    WebViewConfiguration webViewConfiguration,
-    String? webOnlyWindowName,
-  });
-}
-
-late LaunchUrlFunction mockLaunchUrl;
+class MockUrlLauncher extends Mock implements UrlLauncher {}
 
 late AppCubit cubit;
 AppState get state => cubit.state;
 
 void main() {
+  late UrlLauncher urlLauncher;
+
   setUpAll(() {
     registerFallbackValue(Uri());
   });
 
   setUp(() {
-    mockCanLaunchUrl = MockCanLaunchUrl();
-    when(() => mockCanLaunchUrl(any())).thenAnswer((_) async => true);
-    mockLaunchUrl = MockLaunchUrl();
-    when(() => mockLaunchUrl(any())).thenAnswer((_) async => true);
+    urlLauncher = MockUrlLauncher();
+    when(() => urlLauncher.canLaunch(any())).thenAnswer((_) async => true);
+    when(() => urlLauncher.launch(any())).thenAnswer((_) async => true);
 
     cubit = AppCubit(
-      mockCanLaunchUrl,
-      mockLaunchUrl,
+      urlLauncher,
     );
   });
 
@@ -47,18 +32,18 @@ void main() {
       test('launches a correct url', () async {
         final result = await cubit.launchURL(correctTestUrl);
         expect(result, true);
-        verify(() => mockCanLaunchUrl(any())).called(1);
-        verify(() => mockLaunchUrl(any())).called(1);
+        verify(() => urlLauncher.canLaunch(any())).called(1);
+        verify(() => urlLauncher.launch(any())).called(1);
       });
 
       test('bad url returns false', () async {
         const badTestUrl = 'htp:/gogg.m';
-        when(() => mockCanLaunchUrl(Uri.parse(badTestUrl)))
+        when(() => urlLauncher.canLaunch(Uri.parse(badTestUrl)))
             .thenAnswer((_) async => false);
         final result = await cubit.launchURL(badTestUrl);
         expect(result, false);
-        verify(() => mockCanLaunchUrl(any())).called(1);
-        verifyNever(() => mockLaunchUrl(any()));
+        verify(() => urlLauncher.canLaunch(any())).called(1);
+        verifyNever(() => urlLauncher.launch(any()));
       });
     });
   });
