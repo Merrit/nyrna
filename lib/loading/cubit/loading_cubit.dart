901e4cc0e8af52017cf1fbd30bad535ce480c5da
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-01-18 15:53:48 -0500

    feat: add check and warning for Wayland session

--- lib/loading/cubit/loading_cubit.dart
+++ lib/loading/cubit/loading_cubit.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../logs/logs.dart';
 import '../../native_platform/native_platform.dart';
+import '../../native_platform/src/linux/linux.dart';
 
 part 'loading_state.dart';
 
@@ -10,13 +14,29 @@ class LoadingCubit extends Cubit<LoadingState> {
 
   LoadingCubit()
       : nativePlatform = NativePlatform(),
-        super(LoadingInitial()) {
+        super(const LoadingInitial()) {
     checkDependencies();
   }
 
   Future<void> checkDependencies() async {
+    log.v('Checking dependencies..');
+
+    // Make sure we are not running under Wayland.
+    if (Platform.isLinux) {
+      final sessionType = await (nativePlatform as Linux).sessionType();
+      if (sessionType != 'x11') {
+        emit(const LoadingWaylandError());
+        return;
+      }
+    }
+
     final dependenciesSatisfied = await nativePlatform.checkDependencies();
-    final newState = dependenciesSatisfied ? LoadingSuccess() : LoadingFailed();
+
+    LoadingState newState;
+    newState = (dependenciesSatisfied) //
+        ? const LoadingSuccess()
+        : const LoadingDependencyError();
+
     emit(newState);
   }
 }
