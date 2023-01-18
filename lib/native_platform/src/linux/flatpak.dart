d570e7b501c5ca0e75534c23d7f4f89e08ec9dd6
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-01-18 15:52:12 -0500

    feat: add verbose logs for flatpak run

--- lib/native_platform/src/linux/flatpak.dart
+++ lib/native_platform/src/linux/flatpak.dart
@@ -1,13 +1,27 @@
 import 'dart:io';
 
+import '../../../logs/logs.dart';
 import '../typedefs.dart';
 
 /// Run commands from within a Flatpak environment, directly on the host.
 RunFunction flatpakRun = (String executable, List<String> args) async {
-  return await Process.run(
+  log.v('''
+Running command from flatpak; using flatpakRun..
+executable: $executable
+args: $args''');
+
+  final result = await Process.run(
     'flatpak-spawn',
     ['--host', executable, ...args],
   );
+
+  final error = result.stderr.toString().trim();
+
+  if (error != '') {
+    log.e('Issue running command through flatpak-spawn: $error');
+  }
+
+  return result;
 };
 
 /// When in Flatpak using the built-in Process.killPid will try to act on the
