e5c84d268431d6832352feea9f0a74f0c67cb5a4
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-07-03 16:27:59 -0400

    refactor: migrate app_cubit_test to mockito

--- test/app/cubit/app_cubit_test.dart
+++ test/app/cubit/app_cubit_test.dart
@@ -1,44 +1,30 @@
-import 'package:flutter/services.dart';
-import 'package:mocktail/mocktail.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:nyrna/app/app.dart';
 import 'package:nyrna/logs/logs.dart';
 import 'package:nyrna/storage/storage_repository.dart';
 import 'package:test/test.dart';
-import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
-class MockStorageRepository extends Mock implements StorageRepository {}
+@GenerateNiceMocks(<MockSpec>[
+  MockSpec<StorageRepository>(),
+])
+import 'app_cubit_test.mocks.dart';
 
-class MockLaunchUrl extends Mock {
-  Future<bool> call(
-    Uri url, {
-    url_launcher.LaunchMode mode,
-    url_launcher.WebViewConfiguration webViewConfiguration,
-    String? webOnlyWindowName,
-  });
-}
+MockStorageRepository mockStorageRepository = MockStorageRepository();
 
 late AppCubit cubit;
 AppState get state => cubit.state;
 
 void main() {
-  late StorageRepository storageRepository;
-  late LaunchUrl urlLauncher;
-
   setUpAll(() async {
-    registerFallbackValue(Uri());
     await LoggingManager.initialize(verbose: false);
   });
 
   setUp(() {
-    storageRepository = MockStorageRepository();
-    when(() => storageRepository.getValue(any())).thenAnswer((_) async => null);
-
-    urlLauncher = MockLaunchUrl();
-    when(() => urlLauncher.call(any())).thenAnswer((_) async => true);
+    reset(mockStorageRepository);
 
     cubit = AppCubit(
-      storageRepository,
-      urlLauncher,
+      mockStorageRepository,
     );
   });
 
@@ -46,26 +32,6 @@ void main() {
     test('firstRun default is true', () {
       // This test may require a delay if the cubit's init takes longer.
       expect(state.firstRun, true);
-    });
-
-    group('launchURL:', () {
-      const correctTestUrl = 'https://www.google.com/';
-
-      test('launches a correct url', () async {
-        final result = await cubit.launchURL(correctTestUrl);
-        expect(result, true);
-        verify(() => urlLauncher.call(any())).called(1);
-      });
-
-      test('bad url returns false', () async {
-        const badTestUrl = 'htp:/gogg.m';
-        final uri = Uri.parse(badTestUrl);
-        when(() => urlLauncher.call(uri))
-            .thenThrow(PlatformException(code: 'bad url'));
-        final result = await cubit.launchURL(badTestUrl);
-        expect(result, false);
-        verify(() => urlLauncher.call(any())).called(1);
-      });
     });
   });
 }
