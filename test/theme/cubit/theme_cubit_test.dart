a060f9e04baf09d29b779d96636dd3d4bff7e8fd
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-07-03 17:17:23 -0400

    refactor: migrate theme_cubit_test to mockito

--- test/theme/cubit/theme_cubit_test.dart
+++ test/theme/cubit/theme_cubit_test.dart
@@ -1,11 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:nyrna/storage/storage_repository.dart';
 import 'package:nyrna/theme/theme.dart';
 
-class MockStorageRepository extends Mock implements StorageRepository {}
+@GenerateNiceMocks(<MockSpec>[
+  MockSpec<StorageRepository>(),
+])
+import 'theme_cubit_test.mocks.dart';
 
-StorageRepository storage = MockStorageRepository();
+final mockStorageRepo = MockStorageRepository();
 
 late ThemeCubit cubit;
 ThemeState get state => cubit.state;
@@ -13,14 +17,16 @@ ThemeState get state => cubit.state;
 void main() {
   group('ThemeCubit:', () {
     setUp(() async {
-      when(() => storage.getValue(any())).thenAnswer((_) async {});
-      when(() => storage.saveValue(
-            key: any(named: 'key'),
-            value: any(named: 'value'),
-            storageArea: any(named: 'storageArea'),
-          )).thenAnswer((_) async {});
+      reset(mockStorageRepo);
 
-      cubit = await ThemeCubit.init(storage);
+      when(mockStorageRepo.getValue(any)).thenAnswer((_) async {});
+      when(mockStorageRepo.saveValue(
+        key: anyNamed('key'),
+        value: anyNamed('value'),
+        storageArea: anyNamed('storageArea'),
+      )).thenAnswer((_) async {});
+
+      cubit = await ThemeCubit.init(mockStorageRepo);
     });
 
     test('global instance is available', () {
@@ -32,24 +38,24 @@ void main() {
     });
 
     test('saved light theme preference loads light theme', () async {
-      when(() => storage.getValue('appTheme'))
+      when(mockStorageRepo.getValue('appTheme'))
           .thenAnswer((_) async => 'AppTheme.light');
-      cubit = await ThemeCubit.init(storage);
+      cubit = await ThemeCubit.init(mockStorageRepo);
       expect(state.appTheme, AppTheme.light);
     });
 
     test('saved dark theme preference loads dark theme', () async {
-      when(() => storage.getValue('appTheme'))
+      when(mockStorageRepo.getValue('appTheme'))
           .thenAnswer((_) async => 'AppTheme.dark');
-      cubit = await ThemeCubit.init(storage);
+      cubit = await ThemeCubit.init(mockStorageRepo);
       expect(state.appTheme, AppTheme.dark);
     });
 
     test('saved pitch black theme preference loads pitch black theme',
         () async {
-      when(() => storage.getValue('appTheme'))
+      when(mockStorageRepo.getValue('appTheme'))
           .thenAnswer((_) async => 'AppTheme.pitchBlack');
-      cubit = await ThemeCubit.init(storage);
+      cubit = await ThemeCubit.init(mockStorageRepo);
       expect(state.appTheme, AppTheme.pitchBlack);
     });
 
@@ -59,24 +65,24 @@ void main() {
       // Light
       await cubit.changeTheme(AppTheme.light);
       expect(state.appTheme, AppTheme.light);
-      verify(() => storage.saveValue(
-            key: 'appTheme',
-            value: 'AppTheme.light',
-          )).called(1);
+      verify(mockStorageRepo.saveValue(
+        key: 'appTheme',
+        value: 'AppTheme.light',
+      )).called(1);
       // Dark
       await cubit.changeTheme(AppTheme.dark);
       expect(state.appTheme, AppTheme.dark);
-      verify(() => storage.saveValue(
-            key: 'appTheme',
-            value: 'AppTheme.dark',
-          )).called(1);
+      verify(mockStorageRepo.saveValue(
+        key: 'appTheme',
+        value: 'AppTheme.dark',
+      )).called(1);
       // Pitch Black
       await cubit.changeTheme(AppTheme.pitchBlack);
       expect(state.appTheme, AppTheme.pitchBlack);
-      verify(() => storage.saveValue(
-            key: 'appTheme',
-            value: 'AppTheme.pitchBlack',
-          )).called(1);
+      verify(mockStorageRepo.saveValue(
+        key: 'appTheme',
+        value: 'AppTheme.pitchBlack',
+      )).called(1);
     });
   });
 }
