a9b8804c109c5994a4ed382a19decd6e0aa363db
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-07-05 13:30:35 -0400

    refactor: migrate Process to freezed

--- lib/native_platform/src/process/models/process.dart
+++ lib/native_platform/src/process/models/process.dart
@@ -1,46 +1,34 @@
-import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'process.freezed.dart';
 
 /// Current status of a process.
 enum ProcessStatus {
+  /// The process is running normally.
+  // @JsonValue('normal')
   normal,
+
+  /// The process is suspended.
+  // @JsonValue('suspended')
   suspended,
+
+  /// The process status is unknown.
+  // @JsonValue('unknown')
   unknown,
 }
 
 /// Represents a running process on the host system.
-class Process extends Equatable {
-  /// Name of the executable, for example 'firefox' or 'firefox-bin'.
-  final String executable;
+@freezed
+class Process with _$Process {
+  const factory Process({
+    /// Name of the executable, for example 'firefox' or 'firefox-bin'.
+    required String executable,
 
-  /// The Process ID (PID) of the given process.
-  final int pid;
+    /// The Process ID (PID) of the given process.
+    required int pid,
 
-  /// Status will be one of [ProcessStatus.normal],
-  /// [ProcessStatus.suspended] or [ProcessStatus.unknown].
-  final ProcessStatus status;
-
-  const Process({
-    required this.executable,
-    required this.pid,
-    required this.status,
-  });
-
-  Process copyWith({
-    String? executable,
-    int? pid,
-    ProcessStatus? status,
-  }) {
-    return Process(
-      executable: executable ?? this.executable,
-      pid: pid ?? this.pid,
-      status: status ?? this.status,
-    );
-  }
-
-  @override
-  String toString() =>
-      'Process(executable: $executable, pid: $pid, status: $status)';
-
-  @override
-  List<Object> get props => [executable, pid, status];
+    /// Status will be one of [ProcessStatus.normal],
+    /// [ProcessStatus.suspended] or [ProcessStatus.unknown].
+    required ProcessStatus status,
+  }) = _Process;
 }
