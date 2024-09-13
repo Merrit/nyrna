8d09624a6a9612af96efaad5aeaa6c5b884071bc
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2024-09-13 15:39:15 -0400

    ci: skip problematic test on Wayland

--- test/native_platform/src/native_platform_test.dart
+++ test/native_platform/src/native_platform_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io' as io;
+
 import 'package:nyrna/native_platform/native_platform.dart';
 import 'package:test/test.dart';
 
@@ -15,6 +17,13 @@ void main() {
     });
 
     test('activeWindow returns a window', () async {
+      // If system is Wayland, skip this test since it will fail.
+      // Might be able to revisit once Wayland support is added.
+      final sessionType = io.Platform.environment['XDG_SESSION_TYPE'];
+      if (sessionType == 'wayland') {
+        return;
+      }
+
       final activeWindow = await platform.activeWindow();
       expect(activeWindow.id, isPositive);
       expect(activeWindow.process.pid, isPositive);
