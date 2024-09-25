343a473c31f045bd926c8bcd0ad39296a2517926
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2024-09-25 15:07:35 -0400

    feat: show errors in banner to make them more visible

--- lib/apps_list/widgets/widgets.dart
+++ lib/apps_list/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'custom_app_bar.dart';
 export 'first_run_dialog.dart';
+export 'nyrna_error_message.dart';
 export 'window_tile.dart';
--- lib/apps_list/widgets/window_tile.dart
+++ lib/apps_list/widgets/window_tile.dart
@@ -1,11 +1,8 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import '../../app/app.dart';
 import '../../logs/logs.dart';
 import '../../native_platform/native_platform.dart';
 import '../apps_list.dart';
@@ -147,16 +144,6 @@ class _DetailsButton extends StatelessWidget {
 
     return BlocBuilder<AppsListCubit, AppsListState>(
       builder: (context, state) {
-        final interactionError = state //
-            .interactionErrors
-            .singleWhereOrNull((element) => element.windowId == window.id);
-
-        final errorText = (interactionError != null) ? '❗' : '';
-        final Widget errorIndicator = Text(
-          errorText,
-          style: const TextStyle(fontFamily: 'Noto Color Emoji'),
-        );
-
         final toggleAllButton = MenuItemButton(
           child: Text('$availableAction all instances'),
           onPressed: () => context.read<AppsListCubit>().toggleAll(window),
@@ -182,7 +169,7 @@ class _DetailsButton extends StatelessWidget {
                 showDialog(
                   context: context,
                   builder: (context) {
-                    return _DetailsDialog(interactionError, window);
+                    return _DetailsDialog(window);
                   },
                 );
               },
@@ -194,7 +181,6 @@ class _DetailsButton extends StatelessWidget {
         return Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            errorIndicator,
             moreActionsButton,
           ],
         );
@@ -204,10 +190,9 @@ class _DetailsButton extends StatelessWidget {
 }
 
 class _DetailsDialog extends StatelessWidget {
-  final InteractionError? interactionError;
   final Window window;
 
-  const _DetailsDialog(this.interactionError, this.window);
+  const _DetailsDialog(this.window);
 
   @override
   Widget build(BuildContext context) {
@@ -217,35 +202,36 @@ class _DetailsDialog extends StatelessWidget {
           AppLocalizations.of(context)!.detailsDialogTitle,
         ),
       ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          _ErrorMessage(interactionError),
-          ListTile(
-            title: Text(
-              AppLocalizations.of(context)!.detailsDialogWindowTitle,
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: Text(
+                AppLocalizations.of(context)!.detailsDialogWindowTitle,
+              ),
+              subtitle: SelectableText(window.title),
             ),
-            subtitle: SelectableText(window.title),
-          ),
-          ListTile(
-            title: Text(
-              AppLocalizations.of(context)!.detailsDialogExecutableName,
+            ListTile(
+              title: Text(
+                AppLocalizations.of(context)!.detailsDialogExecutableName,
+              ),
+              subtitle: SelectableText(window.process.executable),
             ),
-            subtitle: SelectableText(window.process.executable),
-          ),
-          ListTile(
-            title: Text(
-              AppLocalizations.of(context)!.detailsDialogPID,
+            ListTile(
+              title: Text(
+                AppLocalizations.of(context)!.detailsDialogPID,
+              ),
+              subtitle: SelectableText(window.process.pid.toString()),
             ),
-            subtitle: SelectableText(window.process.pid.toString()),
-          ),
-          ListTile(
-            title: Text(
-              AppLocalizations.of(context)!.detailsDialogCurrentStatus,
+            ListTile(
+              title: Text(
+                AppLocalizations.of(context)!.detailsDialogCurrentStatus,
+              ),
+              subtitle: SelectableText(window.process.status.name),
             ),
-            subtitle: SelectableText(window.process.status.name),
-          ),
-        ],
+          ],
+        ),
       ),
       actions: [
         TextButton(
@@ -253,80 +239,6 @@ class _DetailsDialog extends StatelessWidget {
           child: const Text('Close'),
         ),
       ],
-    );
-  }
-}
-
-class _ErrorMessage extends StatelessWidget {
-  final InteractionError? interactionError;
-
-  const _ErrorMessage(this.interactionError);
-
-  @override
-  Widget build(BuildContext context) {
-    if (interactionError == null) return const SizedBox();
-
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.red),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Card(
-        child: ExpansionTile(
-          leading: const Text(
-            '❗',
-            style: TextStyle(fontFamily: 'Noto Color Emoji'),
-          ),
-          title: Text(
-            'Encountered a problem attempting to ${interactionError!.interactionType.name}.',
-          ),
-          children: [
-            const Text(
-              '''
-This is sometimes resolved by running Nyrna with root / administrator privileges.
-
-Interacting with applications can go wrong for several reasons. In general, manipulating processes like this is known to be able to cause unpredictable results and there is usually not much Nyrna can do if it responds poorly, not at all, or crashes.
-
-This is why Nyrna comes with the disclaimer that things can go wrong, and you should be sure to always save your work and games.''',
-            ),
-            const Divider(),
-            const Text('More info'),
-            ExpansionTile(
-              title: const Text('If this is a game...'),
-              children: [
-                MarkdownBody(
-                  data: '''
-If this is a game, check if it uses `Easy Anti-Cheat` by searching for it at [pcgamingwiki.com](https://www.pcgamingwiki.com) and checking if the "Middleware" section lists Easy Anti-Cheat.
-
-Due to the restricted and obfuscated nature of Easy Anti-Cheat Nyrna cannot manage titles that use this.''',
-                  onTapLink: (String text, String? href, String title) {
-                    if (href == null) return;
-                    AppCubit.instance.launchURL(href);
-                  },
-                ),
-              ],
-            ),
-            ExpansionTile(
-              title: const Text('Report bug'),
-              children: [
-                MarkdownBody(
-                  data: '''
-If you believe this is an issue with Nyrna rather than a limitation of manipulating processes you can [create an issue](https://github.com/Merrit/nyrna/issues).
-
-For troubleshooting or to include in filing an issue you can obtain more detailed logs by:
-- Starting Nyrna with verbose logging, eg: `nyrna --verbose`
-- Reproducing the error
-- Copying the logs from the settings page''',
-                  onTapLink: (String text, String? href, String title) {
-                    if (href == null) return;
-                    AppCubit.instance.launchURL(href);
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
